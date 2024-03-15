af69207336c7e25993bbbe7cb4e4c77b2952cacf
Paseval <111348353+azure1033@users.noreply.github.com>
2024-03-15 12:25:51 +0800

    Update and rename leetcode to leetcode.py

---
+++ leetcode.py
@@ -0,0 +1,15 @@
+
+nums = [5, 3, 3]
+def f(l):
+    for i in range(len(l)):
+        while i + 1 < len(l) and l[i] <= l[i + 1]:
+            m = l[i] + l[i + 1]
+            l[i + 1] = m
+            l.pop(i)
+            print(l)
+            i += 1
+        else:
+            pass
+while len(nums) >= 2:
+    f(nums)
+print(max(nums))
